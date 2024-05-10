206143e32e838c7d739a2bebc60544ff4c49df3c
listenGrey <tinghui561@outlook.com>
2024-05-10 22:49:43 +0800

    add kafka groups

--- controller/kafka.go
+++ controller/kafka.go
@@ -14,30 +14,49 @@ import (
 // RegisterService 用户注册
 func RegisterService(address string) error {
 	ctx := context.Background()
-	reader := kafka.NewReader(kafka.ReaderConfig{
-		Brokers:        []string{address},
-		Topic:          "register",
-		CommitInterval: 1 * time.Second,
-		GroupID:        "rec_team",
-		StartOffset:    kafka.FirstOffset,
-	})
 
-	fmt.Println(" kafka 服务正在运行")
+	// 创建 10 个 Kafka 消费者组，每个消费者组一个协程
+	for i := 0; i < 10; i++ {
+		go func(groupID string) {
+			reader := kafka.NewReader(kafka.ReaderConfig{
+				Brokers:        []string{address},
+				Topic:          "register",
+				CommitInterval: 1 * time.Second,
+				GroupID:        groupID,
+				StartOffset:    kafka.FirstOffset,
+			})
 
-	for {
-		ms, err := reader.ReadMessage(ctx)
-		if err != nil {
-			return err
-		}
-		var user model.User
-		err = json.Unmarshal(ms.Value, &user)
-		if err != nil {
-			return err
-		}
+			fmt.Printf("Consumer with GroupID %s is running\n", groupID)
 
-		err = dao.Register(&user)
-		if err != nil {
-			return err
-		}
+			for {
+				ms, err := reader.ReadMessage(ctx)
+				if err != nil {
+					fmt.Printf("Error reading message for GroupID %s: %s\n", groupID, err)
+					continue
+				}
+
+				var user model.User
+				err = json.Unmarshal(ms.Value, &user)
+				if err != nil {
+					fmt.Printf("Error unmarshalling message for GroupID %s: %s\n", groupID, err)
+					continue
+				}
+
+				// 处理消息
+				err = dao.Register(&user)
+				if err != nil {
+					fmt.Printf("Error registering user for GroupID %s: %s\n", groupID, err)
+					continue
+				}
+
+				// 手动提交消息的偏移量
+				if err = reader.CommitMessages(ctx, ms); err != nil {
+					fmt.Printf("Error committing offset for GroupID %s: %s\n", groupID, err)
+				}
+			}
+		}(fmt.Sprintf("register_%d", i+1))
 	}
+
+	// 阻塞主 goroutine
+	select {}
 }
